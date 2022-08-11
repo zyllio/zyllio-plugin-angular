26e7931e3838b5502fb64cb4c7c1aa9ebd28ffaa
Zyllio <96659995+zyllio@users.noreply.github.com>
2022-08-11 22:39:19 +0200

    Moved to NG14 and SDK 1.0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, ElementRef, Input, OnChanges, SimpleChanges, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  // selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   encapsulation: ViewEncapsulation.ShadowDom
