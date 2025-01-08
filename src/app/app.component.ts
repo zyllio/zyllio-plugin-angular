9adfd2b01b0b9cf2b4355c6900b43506bec4aaa5
Zyllio <96659995+zyllio@users.noreply.github.com>
2025-01-08 22:03:32 +0100

    -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,11 @@
 import { Component, ViewEncapsulation, input, model, output } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { ListColumnItemModel, ListColumnItemsModel } from '@zyllio/zy-sdk';
 
 @Component({
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  standalone: true,
-  encapsulation: ViewEncapsulation.ShadowDom,
-  imports: [
-    BrowserModule
-  ],
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    encapsulation: ViewEncapsulation.ShadowDom,
+    imports: []
 })
 export class AppComponent {
 
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { createApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { createCustomElement } from '@angular/elements';
 import { TimelineMetadata } from './app/metadata';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 
 const run = async () => {
-  
-  const applicationRef = await createApplication()
+
+  const applicationRef = await createApplication({
+    providers: [provideExperimentalZonelessChangeDetection()]
+  })
 
   const TimelineComponent = createCustomElement(AppComponent, { injector: applicationRef.injector })
 
